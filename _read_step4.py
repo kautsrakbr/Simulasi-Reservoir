b59620e343f5815d3e1227a8709e63ca1e535705
Muhamad Alkautsar Akbar Marfiadi <kautsrakbr@gmail.com>
2026-06-24 23:53:20 +0700

    Final Before Submit

--- _read_step4.py
+++
@@ -1,9 +0,0 @@
-import json, sys
-sys.stdout.reconfigure(encoding='utf-8')
-
-with open('workflow.ipynb', 'r', encoding='utf-8') as f:
-    nb = json.load(f)
-
-cell = next(c for c in nb['cells'] if c.get('id') == 'aef0691a')
-src = ''.join(cell['source'])
-print(src)
